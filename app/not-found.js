8c468dd6dce0ec405c09fcd8446175db34a80558
Hasnain Khushi <hasnainkhushii@gmail.com>
2026-07-09 04:52:51 +0500

    working on beckend

--- app/not-found.js
+++ app/not-found.js
@@ -1,11 +1,9 @@
-import React from 'react'
-
-const notfound = () => {
+export default function NotFound() {
   return (
     <div>
-        <h1 className='text-white text-3xl font-bold text-center mt-20'>404 - Page Not Found</h1>   
+      <h1 className="text-white text-3xl font-bold text-center mt-20">
+        404 - Page Not Found
+      </h1>
     </div>
-  )
+  );
 }
-
-export default notfound
